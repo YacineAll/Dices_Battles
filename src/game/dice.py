d657fe0906fca953baa6c62302d4b6f6ce0c3a42
ALLOUACHE <3701222@ppti-14-401-11.ufr-info-p6.jussieu.fr>
2019-11-21 18:46:19 +0100

    fifth commit
    Yacine

--- src/game/dice.py
+++ src/game/dice.py
@@ -4,9 +4,12 @@
 Created on Wed Nov 20 17:34:39 2019
 
 @author: 3701222
-"""
-from random import randint
-sides = 6 
 
-def dice(): 
+MOGPL Project
+"""
+
+from random import randint
+sides = 6
+
+def dice():
     return randint(1,sides)
