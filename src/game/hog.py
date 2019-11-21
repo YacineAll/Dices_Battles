373d33a62d0bbb1617bbcaaaf7d7fb5f28c80e91
Yacine Allouache <yacine.allouache@etu.upmc.fr>
2019-11-21 22:43:17 +0100

    ssh

--- src/game/hog.py
+++ src/game/hog.py
@@ -12,25 +12,25 @@ from dice import *
 SCORE_MAX = 100
 D_MAX = 10
 def _roulez_les_des(d):
-    
+
     assert type(d) == int, 'nombre de dés doit étre un entier.'
     assert d > 0, 'il faut faire au moin un lacer.'
-    
+
     vfunc = np.vectorize(lambda x: dice())
-    a = np.zeros(d) - 1 
-    a = vfunc(a) 
+    a = np.zeros(d) - 1
+    a = vfunc(a)
     if(1 in a):
         return 1
     else:
         return a.sum()
-    
-    
+
+
 def tour(d, score_adverse):
     """Simuler un tour en lançant d dés.
     d:       Le nombre des lancers des dés qui seront faits.
     score_adverse:  Le score total de l'adversaire.
     """
-    
+
     assert type(d) == int, 'd doit être un entier.'
     assert d > 0, 'Impossible de lancer un nombre négatif de dés ou zero .'
     assert d <= 10, 'ON ne peut pas lancer plus de 10 dés.'
@@ -46,36 +46,37 @@ def autre(q):
 def jouer(strategie0, strategie1, but = SCORE_MAX):
     """Simulez une partie et renvoyez les scores finaux des deux joueurs, avec
     Le score du joueur 0 en premier et celui du joueur 1 en second.
-    
+
     Une stratégie est une fonction qui prend deux scores totaux comme arguments
     (le score du joueur actuel et le score de l'adversaire), et renvoie un
     nombre de dés que le joueur actuel lancera a ce tour.
-    
+
     strategie0: fonction de stratégie pour le joueur 0, qui joue en premier.
     strategie1: fonction de stratégie pour le joueur 1, qui joue en second.
-    
+
     """
-        
+
     score, score_adverse = 0, 0
-    
+
     while score < but and score_adverse < but :
         p0 = strategie0(score,score_adverse)
         current_score0 = tour(p0, score_adverse)
-        
+
         score += current_score0
-        
+
         if score >=100:
             return score, score_adverse
-        
+
         p1 = strategie1(score_adverse, score)
+
         current_score1 = tour(p1, score)
-        
+
         score_adverse += current_score1
-    
+
     return score, score_adverse
 
 
-        
+
 def toujour_lancer(n):
     """Renvoie une stratégie qui lance toujours N dés.
     Une stratégie est une fonction qui prend deux scores totaux comme arguments
@@ -89,11 +90,11 @@ def toujour_lancer(n):
     """
     def strategie(score, score_adverse):
         return n
-    return strategie        
+    return strategie
 
 
 
-def max_esperance():
+def strategie_aveugle():
     """Renvoie le nombre de dés (1 à 10) donnant le tour moyen le plus élevé.
     EP = max ( 4*d*np.power(5/6,d) + 1 - np.power(5/6,d) for d in {1,2,3,...,D_MAX})
     D_MAX : le nombre maximum de des que on peut lancer
@@ -101,40 +102,14 @@ def max_esperance():
     def strategie(score, score_adverse):
         D=D_MAX
         return int(1+np.argmax(np.array([4*d*np.power(5/6,d) + (1-np.power(5/6,d)) for d in range(1,D+1) ])))
-    
-    return strategie
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-def strategie_c(score, score_adverse):
-    
-    def strategie(score,score_adverse):
-        return dice()        
-    
     return strategie
 
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
+
