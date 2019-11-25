8beda387468a05381dadfa93cf860474a34bdda6
Yacine Allouache <yacine.allouache@etu.upmc.fr>
2019-11-25 17:01:00 +0100

    eg

--- src/code.py
+++ src/code.py
@@ -38,7 +38,6 @@ def Q(d,k):
     return a[d][k]
             
 def P(d,k):
-    
     if k == 1:
         return 1.0 - (5.0/6.0)**d
     if (k > 6*d) or (k > 1 and k < 2*d):
@@ -65,17 +64,75 @@ def EP(D=10):
 def G():
     def f(j,k):
         return (k > 0 and j>0)
-    a = np.zeros((N,N))
-    for i in range(1,N):
-        for  j in range(1,N):
-            a[i][j] = max([ probabilite[d][1]*(1- a[j][i-1]) + 
-              sum([ probabilite[d][k]*(1-a[j][i-k]*f(j,i-k) )  for k in range (2*d,6*d + 1) ])  for d in range(1,D+1)])
+    
+    t = N + 6*D 
+    a = np.zeros( ( t, t ))
+    for i in range(1,t):
+        for  j in range(1,t):
+            l = [ probabilite[d][1]*( a[j][i-1]) + sum([ probabilite[d][k] * ( a[j][i-k] * f(j,i-k) ) 
+            for k in range (2*d,6*d + 1) ])  for d in range(1,D+1)]
+            a[i][j] = max(l)
+            #a[i][j][1] = 1+np.argmax(l)
     
     return a
+
+def _esperanceGain():
+    """
+    """
+    n = N
+    d = D
+    eg = np.full((n + 6 * d, n + 6 * d), np.nan)
+    #eg = np.zeros((n + 6 * d, n + 6 * d))
+    opt = np.zeros((n, n), dtype = int)
+        
+    #remplissage des cas de base de l'esperance
+    eg[n: , :n] = 1
+    eg[:n, n: ] = -1
+                    
+    for x in range(n - 1, -1, -1):
+        for y in range(x, -1, -1):
+            for i, j in {(x, y), (y, x)}:
+                #tableau des esperances pour chaque quantité de dés possible :
+                ed = - probabilite[ :, 1: ].dot(eg[j, (i + 1):(i + 6 * d + 1)])
+                print(probabilite[ :, 1: ].dot(eg[j, (i + 1):(i + 6 * d + 1)]))
+                opt[i, j] = ed[1: ].argmax() + 1
+                eg[i, j] =  ed[opt[i, j]]
+    return eg, opt
+
+
+
+
+def _tableProba():
+    """
+    D + 1 car pour avoir des indices de façon plus naturel on rajoute une 
+    colonne 0 et une ligne 0. 
+    """
+    #on va d'abord calculer le tableau des Q(d, k) 
+    #on initialise le tableau à 0
+    q = np.zeros((D + 1, 6 *  D + 1))
+    #on initialise la ligne correspondant à d = 1
+    q[1, 2:7] = 1 / 5
+                
+    #on rempli le tableau q avec les Q(d, k)
+    for di in range(2, D + 1):
+        for k in range(2 * di, 6 * di + 1):
+            q[di, k] = q[di - 1, max(k - 6, 0) : k - 1].sum() / 5
+        
+    #on crée le tableau de probabilités P(d, k)
+    proba = np.zeros((D + 1, 6 * D + 1))
+    #la probabilité d'avoir k = 0 avec 0 dés est égale à 1
+    proba[0, 0] = 1
+    #on initialise la colonne correspondant à k = 1
+    proba[1:, 1] = 1 - (5 / 6) ** np.arange(1, D + 1)
+    #on fait P(d, k) = Q(d, k) * (5/6)**d pour k >= 2
+    proba[:, 2:] = q[:, 2:] * (5 / 6) ** np.arange(D + 1).reshape((D + 1, 1))
+    return proba
+
 
 
 probabilite = generate_P_matrix(D)
 probabilites_matrix = G()
+#eg,opt = _esperanceGain()
 
 
 
