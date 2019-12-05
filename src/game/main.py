eae0ed28bb0807b803cafdbd3e5785f4ba330f72
Yacine Allouache <yacine.allouache@etu.upmc.fr>
2019-12-05 16:20:04 +0100

    strategies

--- src/game/main.py
+++ src/game/main.py
@@ -8,17 +8,46 @@ N = 100
 h = hog(D,N)
 
 
-def moyenne(strategie1,strategie2,steps=1000):
-    def f():
+def moyenne(strategie1,strategie2,name1,name2,steps=1000):
+    def f1():
+        score1,score2 = h.jouer(strategie1,strategie2)
+        return 0 if score1 > score2 else 1
+    
+    def f2():
         score1,score2 = h.jouer(strategie1,strategie2)
         return 0 if score1 > score2 else 1
     
     a = np.zeros((steps))-1
-    vfunc = np.vectorize(lambda x: f())
+    vfunc = np.vectorize(lambda x: f1())
+    s1 = 1 - vfunc(a).sum()/steps
     
-    return 1 - vfunc(a).sum()/steps
+    """
+    a = np.zeros((steps))-1
+    vfunc = np.vectorize(lambda x: f2())
+    s2 = 1 - vfunc(a).sum()/steps
+    """
+    r = dict()
+    t = dict()
+    t[name2] = s1 
+    r[name1] = t
+    """
+    t = dict()
+    t[name1] = s2 
+    r[name2] = t
+    """
+    return r
 
+def function(stratiges,names):
+    from itertools import  combinations_with_replacement
+    l = list(combinations_with_replacement(stratiges,2))
+    names = list(combinations_with_replacement(names,2))
+    
+    r=list()
+    for i in range(len(l)):
+        name1,name2 = names[i]
+        s1,s2       = l[i]
+        r.append(moyenne(s1,s2,name1,name2))
+    return r 
 
 if __name__ == "__main__":
-    a = np.asarray([moyenne(h.strategie_aveugle(),h.strategie_aveugle()) for i in range(0,100)])
-    np.savetxt('data.csv', a, delimiter=',')
+    print("yacine")
